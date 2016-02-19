f9363253fd9eba70dd822b979a06b08ae0c2624f
pdrogfer <pdro74@hotmail.com>
2016-02-19 13:28:43 +0100

    added widget preview

--- app/src/main/java/barqsoft/footballscores/MainActivity.java
+++ app/src/main/java/barqsoft/footballscores/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import barqsoft.footballscores.service.myFetchService;
 
