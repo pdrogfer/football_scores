09473d8c27a32693183476ad6849b2c27b5b861a
pdrogfer <pdro74@hotmail.com>
2016-02-20 18:24:02 +0100

    widget retrieving data properly

--- app/src/main/java/barqsoft/footballscores/widget/WidgetDataProvider.java
+++ app/src/main/java/barqsoft/footballscores/widget/WidgetDataProvider.java
@@ -2,12 +2,20 @@ package barqsoft.footballscores.widget;
 
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Color;
+import android.net.Uri;
+import android.os.Binder;
+import android.util.Log;
 import android.widget.RemoteViews;
 import android.widget.RemoteViewsService;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import barqsoft.footballscores.DatabaseContract;
+import barqsoft.footballscores.ScoresProvider;
 
 /**
  * This acts as the adapter to provide the data to the widget
@@ -15,8 +23,20 @@ import java.util.List;
 public class WidgetDataProvider implements RemoteViewsService.RemoteViewsFactory {
 
     List<String> collection = new ArrayList<>();
+    private Cursor data = null;
     Context context;
     Intent intent;
+    final String[] DB_COLUMNS = {
+            DatabaseContract.scores_table.LEAGUE_COL,
+            DatabaseContract.scores_table.DATE_COL,
+            DatabaseContract.scores_table.TIME_COL,
+            DatabaseContract.scores_table.HOME_COL,
+            DatabaseContract.scores_table.AWAY_COL,
+            DatabaseContract.scores_table.HOME_GOALS_COL,
+            DatabaseContract.scores_table.AWAY_GOALS_COL,
+            DatabaseContract.scores_table.MATCH_ID,
+            DatabaseContract.scores_table.MATCH_DAY
+    };
 
 
     public WidgetDataProvider(Context context, Intent intent) {
@@ -26,10 +46,25 @@ public class WidgetDataProvider implements RemoteViewsService.RemoteViewsFactory
 
     private void initData() {
         collection.clear();
-        // TODO: 19/02/16 provide the actual football data here
-        for (int i = 1; i < 11; i++) {
-            collection.add("ListView item " + i);
-        }
+//        for (int i = 1; i < 11; i++) {
+//            collection.add("ListView item " + i);
+//        }
+//        try {
+//            // TODO: 19/02/16 provide the actual football data here
+//            final long identityToken = Binder.clearCallingIdentity();
+//            ScoresProvider scoresProvider = new ScoresProvider();
+//            Uri fixtures_by_data = DatabaseContract.scores_table.buildScoreWithDate();
+//            data = scoresProvider.query(fixtures_by_data, DB_COLUMNS, null, null,
+//                    DatabaseContract.scores_table.DATE_COL + " ASC");
+//            Binder.restoreCallingIdentity(identityToken);
+//        } catch (Exception e) {
+//            Log.i("app", "Error getting data");
+//        }
+        final long identityToken = Binder.clearCallingIdentity();
+        data = context.getContentResolver().query(
+                DatabaseContract.scores_table.buildScoreWithDate(), null, null, new String[]{"today"}, null);
+        Binder.restoreCallingIdentity(identityToken);
+
     }
 
     @Override
